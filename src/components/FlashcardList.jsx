ae8dd28b80c6c4f9af574dddf037eda9444e0049
Neel Mishra <neelmishra9125@gmail.com>
2024-08-13 09:10:12 +0530

    removed edit buttons from back of card

--- src/components/FlashcardList.jsx
+++ src/components/FlashcardList.jsx
@@ -4,8 +4,8 @@ import Flashcard from './Flashcard'
 
 function FlashcardList({cards,handleRemove}) {
   return (
-    
-    <div className='w-full  h-80 overflow-auto flex justify-center gap-4  flex-wrap-4'>
+      <div className='w-full  h-80 overflow-auto flex  justify-center gap-4  flex-wrap-4'>
+      
     {cards.map((item, index)=>{
         return <Flashcard handleRemove={handleRemove} key={index} id={index} card={item}/>
     })}  
